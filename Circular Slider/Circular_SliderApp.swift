05e73d1a3a328625cec0bd56b6acd550d7a77ec3
Afeez <harfyzz@gmail.com>
2025-02-01 20:04:16 +0000

    Initial Commit

---
+++ Circular Slider/Circular_SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Circular_SliderApp.swift
+//  Circular Slider
+//
+//  Created by Afeez Yunus on 01/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Circular_SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Circular Slider/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Circular Slider
+//
+//  Created by Afeez Yunus on 01/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
