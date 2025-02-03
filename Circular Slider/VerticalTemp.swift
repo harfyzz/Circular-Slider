cac59ce37b653b353e97fd9546c9d7320129221c
Afeez <harfyzz@gmail.com>
2025-02-03 11:03:17 +0000

    -vertical slider

--- Circular Slider/VerticalTemp.swift
+++ Circular Slider/VerticalTemp.swift
@@ -17,121 +17,171 @@ struct VerticalTemp: View {
     @State var isTemperatureActive: Bool = true
     @State var humidity: Double = 0
     @State var lastHumidity: Double = 0
+    var valueText = RiveViewModel(fileName: "valuetext", stateMachineName: "State Machine 1")
     var body: some View {
         
         GeometryReader{ geometry in
-            HStack{
-                VStack{
-                    slider.view()
-                }
-                
-                .onAppear {
-                    viewHeight = geometry.size.height
-                }
-                .onChange(of: isTemperatureActive, { oldValue, newValue in
-                    if isTemperatureActive {
-                        let riveValue = (temperature / 270) * 100
-                        slider.setInput("value", value: riveValue)
-                    } else {
-                        let riveValue = humidity
-                        slider.setInput("value", value: riveValue)
-                    }
-                })
-                .padding(.vertical, 32)
-                .gesture(
-                    DragGesture()
-                        .onChanged { value in
-                            if isTemperatureActive {
-                                if slider.temperature {
-                                    slider.setInput("isActive?", value: true)
-                                    dragOffset = -value.translation.height
-                                    
-                                    // Calculate new temperature based on drag and last temperature
-                                    let dragHeight = dragOffset / viewHeight
-                                    let rawTemp = lastTemperature + (dragHeight * 270)
-                                    temperature = min(max(rawTemp, 0), 270)
-                                    
-                                    // Convert to Rive animation range (0-100)
-                                    let riveValue = (temperature / 270) * 100
-                                    slider.setInput("value", value: riveValue)
-                                }
-                            } else {
-                                if slider.temperature {
-                                    slider.setInput("isActive?", value: true)
-                                    dragOffset = -value.translation.height
-                                    
-                                    // Calculate new temperature based on drag and last temperature
-                                    let dragHeight = dragOffset / viewHeight
-                                    let rawHumidity = lastHumidity + (dragHeight * 100)
-                                    humidity = min(max(rawHumidity, 0), 100)
-                                    
-                                    // Convert to Rive animation range (0-100)
-                                    slider.setInput("value", value: humidity)
-                                }
-                            }
-                            
-                            
-                        }
-                        .onEnded { _ in
-                                    if isTemperatureActive {
-                                        lastTemperature = temperature
-                                    } else {
-                                        lastHumidity = humidity  // Added this line
-                                    }
-                                    dragOffset = 0
-                                    slider.setInput("isActive?", value: false)
-                                }
-                )
-                
-                Spacer()
-                VStack(alignment:.trailing, spacing:32){
+            ZStack(alignment:.leading){
+                HStack{
                     Spacer()
-                    VStack(alignment:.trailing, spacing:8){
-                        HStack{
-                            Image(systemName: "humidity.fill")
-                            Text("Humidity")
+                    VStack(alignment:.trailing, spacing:32){
+                        Spacer()
+                        VStack(alignment:.trailing, spacing:8){
+                            HStack{
+                                Image(systemName: "humidity.fill")
+                                Text("Humidity")
+                            }
+                            .font(.headline)
+                            .fontWeight(.medium)
+                            .foregroundStyle(Color("textTertiary"))
+                            Text(humidity > 3 ? "\(Int(humidity))%" : "Off")
+                                .animation(.spring(duration: 0.2))
+                                .contentTransition(.numericText())
+                                .foregroundStyle(isTemperatureActive ? Color("textTertiary") : Color.white)
+                                .font(.system(size: 48, weight: .semibold, design :.monospaced ))
                         }
-                        .font(.headline)
-                        .fontWeight(.medium)
-                        .foregroundStyle(Color("textTertiary"))
-                        Text(humidity > 3 ? "\(Int(humidity))%" : "Off")
-                            .animation(.spring(duration: 0.2))
-                            .contentTransition(.numericText())
-                            .foregroundStyle(isTemperatureActive ? Color("textTertiary") : Color.white)
-                            .font(.system(size: 48, weight: .semibold, design :.monospaced ))
-                    }
-                    .background(Color("bg"))
-                    .onTapGesture {
-                        withAnimation(.easeIn(duration: 0.2)){
-                            isTemperatureActive = false
+                        .background(Color("bg"))
+                        .onTapGesture {
+                            withAnimation(.easeIn(duration: 0.2)){
+                                isTemperatureActive = false
+                            }
+                        }
+                        VStack(alignment:.trailing, spacing:8){
+                            HStack{
+                                Image(systemName: "drop.degreesign.fill")
+                                Text("Temperature")
+                            }
+                            .font(.headline)
+                            .fontWeight(.medium)
+                            .foregroundStyle(Color("textTertiary"))
+                            Text(temperature > 3 ? "\(Int(temperature))°C" : "Off")
+                                .animation(.spring(duration: 0.2))
+                                .contentTransition(.numericText())
+                                .foregroundStyle(!isTemperatureActive ? Color("textTertiary") : Color.white)
+                                .font(.system(size: 48, weight: .semibold, design :.monospaced ))
+                        }
+                        .background(Color("bg"))
+                        .onTapGesture {
+                            withAnimation(.easeIn(duration: 0.2)){
+                                isTemperatureActive = true
+                            }
                         }
                     }
-                    VStack(alignment:.trailing, spacing:8){
-                        HStack{
-                            Image(systemName: "drop.degreesign.fill")
-                            Text("Temperature")
-                        }
-                        .font(.headline)
-                        .fontWeight(.medium)
-                        .foregroundStyle(Color("textTertiary"))
-                        Text(temperature > 3 ? "\(Int(temperature))°C" : "Off")
-                            .animation(.spring(duration: 0.2))
-                            .contentTransition(.numericText())
-                            .foregroundStyle(!isTemperatureActive ? Color("textTertiary") : Color.white)
-                            .font(.system(size: 48, weight: .semibold, design :.monospaced ))
-                    }
-                    .background(Color("bg"))
-                    .onTapGesture {
-                        withAnimation(.easeIn(duration: 0.2)){
-                            isTemperatureActive = true
-                        }
-                    }
+                    .padding(.bottom, 24)
                 }
-                .padding(.bottom, 24)
-                .frame(maxWidth: .infinity)
-                
+                HStack{
+                    VStack{
+                        slider.view()
+                    }
+                        .frame(width:150 )
+                    .onAppear {
+                        viewHeight = geometry.size.height
+                    }
+                    .onChange(of: isTemperatureActive, { oldValue, newValue in
+                        if isTemperatureActive {
+                            let riveValue = (temperature / 270) * 100
+                            slider.setInput("value", value: riveValue)
+                            do {
+                                try valueText.setTextRunValue("Value", textValue: "\(Int(temperature))°")
+                            } catch {
+                                print ("Cannot set text value")
+                            }
+                        } else {
+                            let riveValue = humidity
+                            slider.setInput("value", value: riveValue)
+                            do {
+                                try valueText.setTextRunValue("Value", textValue:"\(Int(humidity))%")
+                            } catch {
+                                print ("Cannot set text value")
+                            }
+                        }
+                    })
+                    .padding(.vertical, 32)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                if isTemperatureActive {
+                                    if slider.temperature {
+                                        slider.setInput("isActive?", value: true)
+                                        dragOffset = -value.translation.height
+                                        
+                                        // Calculate new temperature based on drag and last temperature
+                                        let dragHeight = dragOffset / viewHeight
+                                        let rawTemp = lastTemperature + (dragHeight * 270)
+                                        temperature = min(max(rawTemp, 0), 270)
+                                        
+                                        // Convert to Rive animation range (0-100)
+                                        let riveValue = (temperature / 270) * 100
+                                        slider.setInput("value", value: riveValue)
+                                        do {
+                                            try valueText.setTextRunValue("Value", textValue: "\(Int(temperature))°" )
+                                        } catch {
+                                            print ("Cannot set text value")
+                                        }
+                                    }
+                                } else {
+                                    if slider.temperature {
+                                        slider.setInput("isActive?", value: true)
+                                        dragOffset = -value.translation.height
+                                        
+                                        // Calculate new temperature based on drag and last temperature
+                                        let dragHeight = dragOffset / viewHeight
+                                        let rawHumidity = lastHumidity + (dragHeight * 100)
+                                        humidity = min(max(rawHumidity, 0), 100)
+                                        
+                                        // Convert to Rive animation range (0-100)
+                                        slider.setInput("value", value: humidity)
+                                        do {
+                                            try valueText.setTextRunValue("Value", textValue: "\(Int(humidity))%" )
+                                        } catch {
+                                            print ("Cannot set text value")
+                                        }
+                                    }
+                                }
+                                
+                                
+                            }
+                            .onEnded { _ in
+                                if isTemperatureActive {
+                                    lastTemperature = temperature
+                                } else {
+                                    lastHumidity = humidity  // Added this line
+                                }
+                                dragOffset = 0
+                                slider.setInput("isActive?", value: false)
+                            }
+                    )
+                    if slider.temperature{
+                        //label text
+                        valueText.view()
+                            .frame(height: 30)
+                            .position(
+                                x: isTemperatureActive ? 10 : 0,
+                                       y: {
+                                           let rawPosition = isTemperatureActive ?
+                                               (viewHeight - 100) - ((temperature / 270) * (viewHeight - 100)) :
+                                               (viewHeight - 100) - ((humidity / 100) * (viewHeight - 100))
+                                           
+                                           // Add padding of ~20-30 pixels from top and bottom
+                                           let topPadding: CGFloat = 48
+                                           let bottomPadding: CGFloat = 85
+                                           
+                                           return min(
+                                               max(rawPosition, topPadding),
+                                               viewHeight - bottomPadding
+                                           )
+                                       }()
+                                   )
+                                   .animation(.spring(), value: temperature)
+                                   .animation(.spring(), value: humidity)
+                            
+                    }
+                            
+                    Spacer()
+                }
+               
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.horizontal, 16)
             .background(Color("bg"))
         }
     }
@@ -158,7 +208,9 @@ class VerticalSlider: RiveViewModel {
             let eventProperties = generalEvent.properties()
             
             if let eventValue = eventProperties["isActive?"] as? Bool {
-                temperature = eventValue
+                withAnimation(.spring(duration:0.2)) {
+                    temperature = eventValue
+                }
             }
         }
     }
